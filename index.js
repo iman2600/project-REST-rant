ed3895b24644e5d171a23fdc18bd82d698fb0700
Isaac Sirstins <113957847+iman2600@users.noreply.github.com>
2023-02-10 22:12:35 -0700

    Adding 404 page view

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 //keep at bottom
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.render("error404");
 });
 
 app.listen(process.env.PORT);
