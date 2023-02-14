eec165942b01173858ed5ce7afbcd27c874f93c5
Isaac Sirstins <113957847+iman2600@users.noreply.github.com>
2023-02-13 19:51:26 -0700

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -21,6 +21,11 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("POST /places");
+});
+
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,6 +1,23 @@
 const React = require("react");
 const Def = require("../default");
 
+let places = [
+  {
+    name: "Generic Pizza Place",
+    city: "Millcreek",
+    state: "UT",
+    cuisines: "Pizza, Wings",
+    pic: "/images/fernando-andrade-_P76trHTWDE-unsplash.jpg",
+  },
+  {
+    name: "Generic Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "images/nathan-dumlao-z3em1GBRhvY-unsplash.jpg",
+  },
+];
+
 function index(data) {
   let placesFormatted = data.places.map((place) => {
     return (
